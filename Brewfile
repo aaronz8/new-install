03302c5f8d0885ebaced2ed79050be81aebbd61d
Aaron Zhang <aaron@thinkero.us>
2015-03-14 13:50:08 -0700

    initial commit

---
+++ Brewfile
@@ -0,0 +1,6 @@
+brew tap homebrew/versions
+brew tap caskroom/versions
+
+install caskroom/cask/brew-cask fasd git-flow httpie mongodb redis
+
+cask install iterm2-nightly sublime-text flux the-unarchiver google-chrome 1password virtualbox slack flowdock screenhero dropbox google-drive flashlight vagrant vagrant-manager spotify skype
---
+++ install.sh
@@ -0,0 +1,27 @@
+# install xcode
+
+# install Prezto
+zsh
+git clone --recursive https://github.com/aaronz8/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
+
+# copy zpreztorc (symlink them)
+cp ./.vimrc ~/
+
+# set homebrew env
+echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.zshenv
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Homebrew packages
+brew bundle Brewfile
+
+# Link `subl` to Sublime
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
+# install node and npm (maybe with brew)
